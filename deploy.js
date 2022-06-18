bb10724d364ad6c846e875b1e6077fffdd41e520
Isaac Martin <isaacamartin@outlook.com>
2022-06-18 17:15:26 -0400

    update .travis.yml and deploy script

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '52.87.249.182',
+      host: '54.175.209.157',
       username: 'ubuntu',
       privateKey: 'betamax.pem'
     })
